da0a8a9ffa4f5bc529f775bc85089156a9155940
Vince Dority <vncdority@gmail.com>
2023-11-21 16:52:18 -0500

    Updated pages folder to include primary pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import About from './pages/About';
 export default function App() {
   return (
-    <div>
-      App
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+  
+
+    
   )
 }
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      Profie
+    </div>
+  )
+}
+
+export default Profile
