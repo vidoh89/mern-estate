c26647cf07bcb53ccb9aba4fe815ee89b02198e7
VINCE DORITY <103743660+vidoh89@users.noreply.github.com>
2023-11-27 15:19:27 -0500

    Add files via upload

---
+++ Issue.md
@@ -0,0 +1,5 @@
+Issue:
+1. .env file not registering when calling element in mongoose.connect()
+
+Status resolved: Removed semicolon from the end of MONGO variable .located on .env file.
+---- 
--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+dotenv.config();
+
+
 
 const app = express();
-
+console.log(process.env);
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to MongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+});
 app.listen(3000,()=>{
 console.log('server is running on port 3000');
-})
+});
+app.use('/api/user',userRouter);
