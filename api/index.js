8da6553109ae2a2e32a548b17494d45b5ab56927
VINCE DORITY <103743660+vidoh89@users.noreply.github.com>
2023-11-28 12:21:55 -0500

    Updated sign-up form for frontend and backend .

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 
-
 const app = express();
-console.log(process.env);
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connected to MongoDB');
 })
@@ -18,3 +18,13 @@ app.listen(3000,()=>{
 console.log('server is running on port 3000');
 });
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+    });
+});
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Signup() {
   return (
     <div>
-      Signup
+      <h1 className = 'text-3xl text-center'>Sign Up</h1>
+      <form className='flex flex-col gap-4 '>
+        <input type='text' placeholder='username'
+        className='border p-3 rounded-lg  text-green-300 bg-opacity-20 text-md' id='username '/>
+        <input type='email' placeholder='email' 
+        className='border p-3 rounded-lg  bg-opacity-20 text-md' id='email '/>
+        <input type='password' placeholder='password'
+        className='border p-3 rounded-lg bg-opacity-20 text-md' id='password '/>
+      </form>
     </div>
   )
 }
